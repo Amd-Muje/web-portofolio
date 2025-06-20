b70a063c7ccd83d4fb6d19789ff8f3de298028a2
ahmadmujahid <ahmadmujahid343@gmail.com>
2025-06-21 02:39:43 +0800

    adding social media bar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,23 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
+import { span } from "framer-motion/client";
+import { MenuItem } from "./menu-items";
+
+const navItems = [
+	{name: "Projects", href: "#work"},
+	{name: "Skills", href: "#skills"},
+	{name: "Experience", href: "#experience"},
+	{name: "Contact", href: "#contact"},
+]
+
+const socialLinks = {
+	github: "",
+	linkedin: "",
+	instagram: "",
+}
+
+
 export default function Navbar() {
 	return (
 		<motion.nav
@@ -10,10 +27,10 @@ export default function Navbar() {
 		>
 			<div className="max-w-7xl mx-auto px-6 py-3">
 				<div className="flex items-center justify-between">
-					{/* <motion.div
+					<motion.div
 						whileHover={{ scale: 1.05 }}
 						className="flex items-center gap-2 group"
-					> */}
+					>
 						<div className="relative h-8 w-8 rounded-full overflow-hidden">
 							<div className="absolute inset-0 bg-gradient-to-r from-primary to-tertiary animate-spin-slow [mask-image:linear-gradient(transparent,white)]" />
 							<div className="absolute inset-[2px] bg-background rounded-full flex items-center justify-center">
@@ -25,7 +42,24 @@ export default function Navbar() {
 						<span className="font-semibold text-content/90 group-hover:text-primary transition-colors">
 							Ahmad Mujahid
 						</span>
-					{/* </motion.div> */}
+					</motion.div>
+					<div className="hidden md:flex items-center gap-6">
+						<div className="flex items-center gap-6 bg-background/80 px-4 py-2 rounded-full border border-white/5 shadow-lg shadow-primary/5">
+							{
+								navItems.map((items, i) => (
+									<MenuItem key={items.name} index={i} href={items.href}>{items.name}</MenuItem>
+								))
+							}
+						</div>
+						<div className="h-6 w-px bg-white/10 mx-2"/>
+						<div className="flex gap-4">
+								<a 
+								href={socialLinks.github}
+								className="p-2 rounded-lg bg-white/5 hover:bg-primary/10 transition-colors group">
+								6
+								</a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</motion.nav>
