4a6f804a7010e54d92429dd39ce26cccfe714775
ahmadmujahid <ahmadmujahid343@gmail.com>
2025-06-22 14:58:28 +0800

    finished

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,12 +2,14 @@
 import React from "react";
 import { motion, useScroll, useTransform} from "framer-motion";
 import Image from "next/image";
+import { ParticleCanvas } from "@/hooks/particle";
 
 export default function Hero() {
 	const {scrollY} = useScroll();
 	const y = useTransform(scrollY, [0, 500], [0,100])
 	return (
 		<section className="min-h-screen relative overflow-hidden bg-black">
+			<ParticleCanvas/>
 			<div className="max-w-7xl mx-auto px-6 pt-32">
 				<div className="flex flex-col lg:flex-row items-center gap-16">
 					<motion.div
